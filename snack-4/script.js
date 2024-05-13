3aa4e08b67b496c4dfb0a896ba5b8be1c4944fd6
CristianN01 <cristiannechifor71@gmail.com>
2024-05-13 12:07:29 +0200

    aggiunto il prompt con la richiesta del Nome

--- snack-4/script.js
+++ snack-4/script.js
@@ -1,3 +1,7 @@
 // In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all'utente il suo nome e comunicagli se può partecipare o no alla festa.
 
 const invitedList = ['Mario','Davide','Luca','Alessio','Giorgio']
+
+const yourName = prompt('Digita il tuo nome')
+
+console.log(yourName)
