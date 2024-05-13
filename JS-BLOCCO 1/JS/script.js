59de36904b58726609b44ff780bf92b59642625a
CristianN01 <cristiannechifor71@gmail.com>
2024-05-13 11:47:27 +0200

    creato primo e secondo  prompt con la risciesta del numero e controllato se funziona

---
+++ JS-BLOCCO 1/JS/script.js
@@ -0,0 +1,6 @@
+// L'utente inserisce due numeri in successione, con due prompt.
+// Il software stampa il maggiore.
+
+const firstNumber = Number.parseInt(prompt('Type first Number'),10);
+const secondNumber = Number.parseInt(prompt('Type second Number'),10);
+console.log(firstNumber, secondNumber );
