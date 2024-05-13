f3af753067760935b3d467ca89a3b3bb02b5b46d
CristianN01 <cristiannechifor71@gmail.com>
2024-05-13 12:35:24 +0200

    aggiunto il cilco di richiesta del numero e la variabile che se il numero e dispari allora viene aggiunto nel listNumber(Array)

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,11 @@
 //  Chiedi per 6 volte all'utente di inserire un numero,
 //  se è dispari inseriscilo nell'array.
 
+const listNumber =['']
+
+for (let i = 0; i < 6; i++){
+    const numberEl =prompt('Type a Number')
+
+    if(numberEl % 2 == 1)
+        listNumber.push(numberEl)
+}
