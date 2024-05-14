04a7f0c4370ed3254424513f6524207814b72295
CristianN01 <cristiannechifor71@gmail.com>
2024-05-14 10:55:29 +0200

    fine esercizio snack-6 creato array e ciclo for che per ogni numero che da resto 0 quindi e pari e viene sommata a tutti quelli pari dentro all'array

--- snack-6/script.js
+++ snack-6/script.js
@@ -1,2 +1,13 @@
 // Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
 
+const pairArray =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let somma = 0;
+
+for( let index = 0 ; index < pairArray.length ; index++){
+
+    if (index % 2 == 0)
+        somma += pairArray[index];
+}
+
+console.log(pairArray, somma)
+
